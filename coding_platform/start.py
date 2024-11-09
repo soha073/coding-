47fb94fb8d0bea05e867d2bb87034a3c58ef5d5f
soha073 <soha.anwar03@gmail.com>
2024-11-09 07:56:55 +0300

    first commit

---
+++ coding_platform/start.py
@@ -0,0 +1,16 @@
+from multiprocessing import Process
+import os
+
+def run_django():
+    os.system("gunicorn your_django_project.wsgi --bind 0.0.0.0:8001")
+
+def run_flask():
+    os.system("gunicorn app:app --bind 0.0.0.0:8000")
+
+if __name__ == "__main__":
+    django_process = Process(target=run_django)
+    flask_process = Process(target=run_flask)
+    django_process.start()
+    flask_process.start()
+    django_process.join()
+    flask_process.join()
